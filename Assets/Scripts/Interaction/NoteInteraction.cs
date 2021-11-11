77a6746da02667e48b59e89837e558fdbb70c393
Amaan Shawkath <amaanshaw@live.co.uk>
2021-11-11 23:18:49 +0000

    creating note interaction

---
+++ Assets/Scripts/Interaction/NoteInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInteraction : CoreInteraction
+{
+    public string noteText;
+    public override void Interact()
+    {
+        UIController.SetupUI(UIType.Note, noteText);
+    }
+}
---
+++ Assets/Scripts/UI/NotePanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NotePanel : CoreUI
+{
+    public TextMeshProUGUI text;
+
+    public override void Setup(params string[] data)
+    {
+        text.text = data[0];
+
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        UIController.HideUI(UIType.Note);
+    }
+}
