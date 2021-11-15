551fff0ce3b35a06e60037ab5a8a1dcca954ce04
Amaan Shawkath <amaanshaw@live.co.uk>
2021-11-15 23:23:19 +0000

    enabling and disabling items based on state

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -17,6 +17,24 @@ public class Item : CoreInteraction
             disabled = true;
 
             base.Interact();
+
+            TriggerEvent(triggerEvent);
+        }
+    }
+
+    public override void OnEvent(params object[] data)
+    {
+        DoorState state = (DoorState)data[0];
+
+        switch(state)
+        {
+            case DoorState.Closed:
+            case DoorState.Locked:
+                disabled = true;
+                break;
+            default:
+                disabled = false;
+                break;
         }
     }
 }
